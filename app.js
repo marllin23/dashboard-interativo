c500c4cd87e5398a78b4e7b179e2c9d16236f916
marllin23 <marllonguerra2310@gmail.com>
2025-04-28 00:53:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
   const ctx = document.getElementById('grafico').getContext('2d');
-  
-const meuGrafico = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
-    datasets: [{
-      label: 'Vendas',
-      data: [12, 19, 3, 5, 2],
-      backgroundColor: [
-        '#42a5f5',
-        '#66bb6a',
-        '#ffa726',
-        '#ab47bc',
-        '#ef5350'
-      ],
-      borderColor: '#1e88e5',
-      borderWidth: 1
-    }]
-  },
-  options: {
-    responsive: true,
-    animation: {
-      duration: 1500, // Duração da animação em milissegundos
-      easing: 'easeInOutQuad', // Tipo de animação
+
+  const meuGrafico = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
+      datasets: [{
+        label: 'Vendas',
+        data: [12, 19, 3, 5, 2],
+        backgroundColor: [
+          '#42a5f5',
+          '#66bb6a',
+          '#ffa726',
+          '#ab47bc',
+          '#ef5350'
+        ],
+        borderColor: '#1e88e5',
+        borderWidth: 1
+      }]
     },
-    scales: {
-      y: {
-        beginAtZero: true
+    options: {
+      responsive: true,
+      animation: {
+        duration: 1500,
+        easing: 'easeInOutQuad',
+      },
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
     }
-  }
+  });
 });
-
